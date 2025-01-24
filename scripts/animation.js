8f8b0cc043dc86af3cde7fa5092c2c4bf237a078
andrei <whoitandrei@yandex.ru>
2025-01-24 17:00:59 +0700

    improve mobile version

--- scripts/animation.js
+++ scripts/animation.js
@@ -46,3 +46,13 @@ document.addEventListener('mouseout', () => {
     // Скрываем круг, если курсор покидает окно
     cursorEffect.style.opacity = '0';
 });
+
+// для раскрывающегося меню
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    menuToggle.addEventListener('click', function () {
+        mainNav.classList.toggle('active'); // Переключаем класс active
+    });
+});
